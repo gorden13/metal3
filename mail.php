f29c900092260a9bc2a4b660665e62a240797595
denis.goryncev <dgoryncev@hculture.ru>
2021-08-26 17:21:26 +0300

    fixses

--- mail.php
+++ mail.php
@@ -14,6 +14,7 @@ $subject = 'Mail subject';
 $headers[] = 'MIME-Version: 1.0';
 $headers[] = 'Content-type: text/html; charset=UTF-8';
 $headers[] = 'From: Biloo <noreply@ydomain.com>';
+$headers[] = 'Access-Control-Allow-Origin: http://localhost:3000/'
 
 mail($to_email, $subject, $message, implode("\r\n", $headers));
 

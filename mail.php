cef06bce0b6766980124daac6daf1444a17e6dbc
denis.goryncev <dgoryncev@hculture.ru>
2021-09-01 13:48:44 +0300

    edrt

--- mail.php
+++ mail.php
@@ -9,12 +9,11 @@ if(isset($_POST['name'])){
 $message = "<p>Hi!</p>";
 $message .= "<p>Wazaaaaa $name</p>";
 
-$to_email = 'konoval777@yandex.ru';
+$to_email = 'svlasov@stoclinic.ru';
 $subject = 'Mail subject';
 $headers[] = 'MIME-Version: 1.0';
 $headers[] = 'Content-type: text/html; charset=UTF-8';
 $headers[] = 'From: Biloo <noreply@ydomain.com>';
-$headers[] = 'Access-Control-Allow-Origin: http://localhost:3000/'
 
 mail($to_email, $subject, $message, implode("\r\n", $headers));
 
